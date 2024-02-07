eafd070927aeb7d98e19c23161969442e775f064
Wasim Alrawas <wassimrawwas@gmail.com>
2024-02-07 18:42:58 +0000

    Remove hardcoded API key, use environment variable

---
+++ asstes/script.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+const inp = document.getElementById('inp')
+const images = document.querySelector('.images')
+
+
+const getImages = () => {
+    console.log('clicked')
+}
